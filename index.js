033394e157e2b8010635bf05caafca89e5014c8c
Randell-P <randellpersad@gmail.com>
2019-04-16 10:57:52 -0400

    Profile Page

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const port=8000
 const app=express()
 const path= require('path');
+app.use('/css',express.static(__dirname +'/pages/css'));
 app.get('/',(req,res)=> res.sendFile(path.join(__dirname + '/pages/index.html')))
+app.get('/profile',(req,res)=> res.sendFile(path.join(__dirname + '/pages/Profile_Page.html')))
 
 app.listen(port, () => {
   console.log('We are live on ' + port);
