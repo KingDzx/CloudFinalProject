93645bbd95b05466400ec4c4fca11103d9c8a838
aundrae <justinramjattan@outlook.com>
2019-04-19 11:25:47 -0400

    Merged all Branches

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const path= require('path');
 app.use('/css',express.static(__dirname +'/pages/css'));
 app.get('/',(req,res)=> res.sendFile(path.join(__dirname + '/pages/index.html')))
 app.get('/profile',(req,res)=> res.sendFile(path.join(__dirname + '/pages/Profile_Page.html')))
+app.get('/registration',(req,res)=> res.sendFile(path.join(__dirname + '/pages/cloud_registration_page.html')))
+app.get('/subscription',(req,res)=> res.sendFile(path.join(__dirname + '/pages/Subscription.html')))
 
 app.listen(port, () => {
   console.log('We are live on ' + port);
